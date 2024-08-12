502ef9a1ba6d2113f2bb2a70e3cded1ee9993117
Eddie <121364681+B-Eddie@users.noreply.github.com>
2024-08-12 17:16:32 -0400

    Update ball.cs

--- ball.cs
+++ ball.cs
@@ -9,6 +9,7 @@ public class Ball : MonoBehaviour
     private Rigidbody2D rb;
     public Text Balle; // Make sure this is assigned in the Inspector
     private List<int> score_num = new List<int> { 0, 0 };
+    private float speedmultiplier = 1f;
 
     void Start()
     {
@@ -24,6 +25,7 @@ public class Ball : MonoBehaviour
 
         Vector2 direction = new Vector2(xDirection, yDirection).normalized;
         rb.velocity = direction * speed;
+        speedmultiplier = 1;
     }
 
     void Update()
@@ -37,30 +39,32 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-
         // Reflect the ball's velocity based on the collision normal
         Vector2 newVelocity = Vector2.Reflect(rb.velocity, collision.contacts[0].normal);
 
         // Maintain the original speed after reflection
-        rb.velocity = newVelocity.normalized * speed;
+        rb.velocity = newVelocity.normalized * speed * speedmultiplier;
 
         if (collision.gameObject.name == "wall1")
-        {
-            score_num[0]++;
-            ResetBall();
-        }
-        else if (collision.gameObject.name == "wall3")
         {
             score_num[1]++;
             ResetBall();
         }
+        else if (collision.gameObject.name == "wall3")
+        {
+            score_num[0]++;
+            ResetBall();
+        }
+
+        speedmultiplier += 0.1f;
+        Debug.Log(speedmultiplier);
     }
 
     public void ResetBall()
     {
         rb.velocity = Vector2.zero;
         transform.position = Vector2.zero;
+        speedmultiplier = 1f;
         LaunchBall();
     }
 }
