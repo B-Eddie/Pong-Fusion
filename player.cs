d8c9b4494259fbdc2bb165fd869fe9d3d1176340
Eddie <121364681+B-Eddie@users.noreply.github.com>
2024-08-12 14:45:15 -0400

    Update player.cs

--- player.cs
+++ player.cs
@@ -7,18 +7,38 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
-            if (transform.position[1] < 3.7) {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.position[1] < 3.7)
+            {
                 transform.position += new Vector3(0, 0.005f, 0);
             }
-        } else if (Input.GetKey(KeyCode.S)) {
-            if (transform.position[1] > -3.7) {
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position[1] > -3.7)
+            {
+                transform.position += new Vector3(0, -0.005f, 0);
+            }
+        }
+        else if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+            float screenWidth = Screen.width;
+
+            if (touchPosition.x < screenWidth / 2)
+            {
+                transform.position += new Vector3(0, 0.005f, 0);
+            }
+            else
+            {
                 transform.position += new Vector3(0, -0.005f, 0);
             }
         }
